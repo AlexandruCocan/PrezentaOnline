543c6c6d6fb2565fab5d898d7b2887a4d177af14
Alex <alexandru.cocan99@gmail.com>
2020-10-25 16:30:40 +0200

    Proiect
    if

---
+++ Proiect/baza_date/connect_cocan.php
@@ -0,0 +1,19 @@
+<?php
+	$user="root";
+	$pass="";
+	$host="localhost:3308";
+	$db="prezenta_online";
+	$con=mysqli_connect($host, $user, $pass, $db) or die("Serverul nu functioneaza!");
+
+
+if() {
+	
+}
+
+	date_default_timezone_set('UTC');
+	$data = date('W');		//saptamana X din an
+	$data_current = ($data-8);	//saptamana curenta
+	$ok_pass = array();
+	$erori = array();
+	 // mysqli_select_db($con,$db) or die("Nu exista baza de date!");
+?>
