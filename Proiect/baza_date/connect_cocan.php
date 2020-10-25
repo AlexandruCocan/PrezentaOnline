17c2801c16b32df8b0415c011503738f2c0b2c3e
Alex <alexandru.cocan99@gmail.com>
2020-10-25 16:32:17 +0200

    connect_cocan
    delete if

--- Proiect/baza_date/connect_cocan.php
+++ Proiect/baza_date/connect_cocan.php
@@ -5,11 +5,6 @@
 	$db="prezenta_online";
 	$con=mysqli_connect($host, $user, $pass, $db) or die("Serverul nu functioneaza!");
 
-
-if() {
-	
-}
-
 	date_default_timezone_set('UTC');
 	$data = date('W');		//saptamana X din an
 	$data_current = ($data-8);	//saptamana curenta
